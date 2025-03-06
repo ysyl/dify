6ed04cb3a2e8cd4be9c070878e0914ee5563a53c
M1 <zhoujian1237@gmail.com>
2025-03-06 11:06:44 +0800

    chat-with-history 默认不展示config panel

--- web/app/components/base/chat/chat-with-history/hooks.tsx
+++ web/app/components/base/chat/chat-with-history/hooks.tsx
@@ -122,7 +122,7 @@ export const useChatWithHistory = (installedAppInfo?: InstalledApp) => {
       })
     }
   }, [appId, conversationIdInfo, setConversationIdInfo])
-  const [showConfigPanelBeforeChat, setShowConfigPanelBeforeChat] = useState(true)
+  const [showConfigPanelBeforeChat, setShowConfigPanelBeforeChat] = useState(false)
 
   const [newConversationId, setNewConversationId] = useState('')
   const chatShouldReloadKey = useMemo(() => {
@@ -254,7 +254,7 @@ export const useChatWithHistory = (installedAppInfo?: InstalledApp) => {
     let hasEmptyInput = ''
     let fileIsUploading = false
     const requiredVars = inputsForms.filter(({ required }) => required)
-    console.log("requiredVars, ", requiredVars)
+    console.log('requiredVars, ', requiredVars)
     if (requiredVars.length) {
       requiredVars.forEach(({ variable, label, type }) => {
         if (hasEmptyInput)
