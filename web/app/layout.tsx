3a8deb4bd7b8cf8d0d7af41728c11e9074364f6c
zjl <zhoujian1237@gmail.com>
2025-03-11 06:23:04 +0000

    修改body默认背景色为bg-background-body

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -36,7 +36,7 @@ const LocaleLayout = ({
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       </head>
       <body
-        className="h-full select-auto color-scheme"
+        className="h-full select-auto color-scheme bg-background-body"
         data-api-prefix={process.env.NEXT_PUBLIC_API_PREFIX}
         data-pubic-api-prefix={process.env.NEXT_PUBLIC_PUBLIC_API_PREFIX}
         data-marketplace-api-prefix={process.env.NEXT_PUBLIC_MARKETPLACE_API_PREFIX}
