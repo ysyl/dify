a87bb23299f4e01073cae16154b75cf7efa0151f
zjl <zhoujian1237@gmail.com>
2025-04-21 07:29:12 +0000

    barInputs初始值调整

--- web/app/components/base/chat/chat-with-history/chat-wrapper.tsx
+++ web/app/components/base/chat/chat-with-history/chat-wrapper.tsx
@@ -122,7 +122,7 @@ const ChatWrapper = ({ chatState, setChatState, hasDigitalHuman }: Props) => {
       return true
     return false
   }, [inputsFormValue, inputsForms])
-  const [barInputs, setBarInputs] = useState<Record<string, any>>({})
+  const [barInputs, setBarInputs] = useState<Record<string, any>>(newConversationInputs)
 
   useEffect(() => {
     if (currentChatInstanceRef.current)
