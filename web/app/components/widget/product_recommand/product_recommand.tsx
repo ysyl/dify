302b76b41fb691e04c89ee2b1558cb8bbe279f81
zjl <zhoujian1237@gmail.com>
2025-06-25 09:47:26 +0000

    chore: 打印productsInfos便于调试

--- web/app/components/widget/product_recommand/product_recommand.tsx
+++ web/app/components/widget/product_recommand/product_recommand.tsx
@@ -43,6 +43,7 @@ function getProductRecommandConfig(node: any): ProductRecommandConfig | null {
     let productsInfos: ProductInfo[] = JSON.parse(
       Buffer.from(productsRawInfosStr, 'base64').toString('utf-8'),
     )
+    console.log('productsInfos after base64 decode:', productsInfos)
     if (version === '2.0')
       productsInfos = productsInfos.map(transformProductInfo)
 
