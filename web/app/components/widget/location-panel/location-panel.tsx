c489c850c7a4c20335a3ec41ea93f879809fa1e8
zjl <zhoujian1237@gmail.com>
2025-07-31 01:42:44 +0000

    去掉交通票组件overflow-hidden, 并调整经纬度测试展示条件

--- web/app/components/widget/location-panel/location-panel.tsx
+++ web/app/components/widget/location-panel/location-panel.tsx
@@ -264,7 +264,7 @@ const LocationPanel: React.FC<{ widgetTagStr?: string, config?: LocationPanelPro
   }
 
   return (
-    <div className="overflow-hidden rounded-lg border p-0 shadow-sm">
+    <div className="rounded-lg border p-0 shadow-sm">
       {/* 自定义header标题 - 添加蓝色背景和图标 */}
       {header && (
         <div className="flex items-center bg-[#1E88E5] p-4 text-white">
@@ -272,7 +272,9 @@ const LocationPanel: React.FC<{ widgetTagStr?: string, config?: LocationPanelPro
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
           </svg>
-          <h3 className="text-lg font-semibold">{header} 经纬度：{latitude}, {longitude}</h3>
+          {
+            getUserLocation && <h3 className="text-lg font-semibold">{header} 经纬度：{latitude}, {longitude}</h3>
+          }
         </div>
       )}
 
